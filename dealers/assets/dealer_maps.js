9272f8033bb1076fd25925a46fddfa3c17674951
Cody Wilson <Cheex11@gmail.com>
2016-08-26 11:14:58 -0700

    added google sheet to pull dealer data from.

---
+++ dealers/assets/dealer_maps.js
@@ -0,0 +1,35 @@
+$( document ).ready(function() {
+
+	init();
+
+    function init() {
+        // Basic options for a simple Google Map
+        // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+        var mapOptions = {
+            // How zoomed in you want the map to start at (always required)
+            zoom: 11,
+
+            // The latitude and longitude to center the map (always required)
+            center: new google.maps.LatLng(47.181033, -122.255857), // New York
+
+            // How you would like to style the map.
+            // This is where you would paste any style found on Snazzy Maps.
+            styles: [{"featureType":"administrative.country","elementType":"geometry","stylers":[{"visibility":"simplified"},{"hue":"#ff0000"}]}]
+        };
+
+        // Get the HTML DOM element that will contain your map
+        // We are using a div with id="map" seen below in the <body>
+        var mapElement = document.getElementById('css-the-map');
+
+        // Create the Google Map using our element and options defined above
+        var map = new google.maps.Map(mapElement, mapOptions);
+
+        // Let's also add a marker while we're at it
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(47.181033, -122.255857),
+            map: map,
+            title: 'Rhino!'
+        });
+    }
+});
+
