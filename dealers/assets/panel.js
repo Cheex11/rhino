db90bcebc49cfd289c256f5c278fa5c6f09d6afb
Cody Wilson <Cheex11@gmail.com>
2016-08-30 16:56:02 -0700

    building out page for no dealers found.

--- dealers/assets/panel.js
+++ dealers/assets/panel.js
@@ -1,4 +1,9 @@
 $( document ).ready(function() {
+
+  var hide_the_map = function() {
+    console.log('i will hide the map!');
+  }
+
   google.maps.event.addDomListener(window, 'load', function() {
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
       center: new google.maps.LatLng(47.186064, -122.253836),
