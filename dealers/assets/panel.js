ffcafa4271e5c3d892e78967f54a181d91dca71d
Cody Wilson <Cheex11@gmail.com>
2016-08-31 09:42:17 -0700

    mobile layout styles added.

--- dealers/assets/panel.js
+++ dealers/assets/panel.js
@@ -1,8 +1,6 @@
 $( document ).ready(function() {
 
-  var hide_the_map = function() {
-    console.log('i will hide the map!');
-  }
+
 
   google.maps.event.addDomListener(window, 'load', function() {
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
@@ -24,4 +22,21 @@ $( document ).ready(function() {
       view: view
     });
   });
+
+
+  $('.map-container-toggle').click(function() {
+    $(this).css('border-bottom', "solid 2px #0080DA");
+    $(this).css('color', "#0080DA");
+    $(this).siblings().css('border-bottom', "none");
+    $(this).siblings().css('color', "#333333");
+
+    var id_to_make_visible = $(this).attr('id').substring(0, $(this).attr('id').indexOf('-toggle'));
+    var id_to_hide = $(this).siblings().attr('id').substring(0, $(this).siblings().attr('id').indexOf('-toggle'));
+
+
+    $('#' + id_to_make_visible + '').css('display','block');
+    $('#' + id_to_hide + '').css('display','none');
+
+  })
+
 });
